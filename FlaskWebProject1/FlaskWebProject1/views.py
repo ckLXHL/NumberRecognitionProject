bb5cf1dc5caaaf4d75ed78b91aaf1de8cf603286
曹凯 <cklxhl@outlook.com>
2019-01-19 01:02:58 +0800

    上传文件与文件列表

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -3,28 +3,29 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from FlaskWebProject1 import app
+from werkzeug import secure_filename
+import os
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
-    return render_template(
-        'index.html',
+    basepath = os.path.dirname(__file__)  # 当前文件所在路径
+    upload_path = os.path.join(basepath, 'static/uploads')
+    return render_template('index.html',
         title='Home Page',
         year=datetime.now().year,
-    )
+        indexs=os.listdir(upload_path))
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
-    return render_template(
-        'contact.html',
+    return render_template('contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
-    )
+        message='Your contact page.')
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
@@ -34,14 +35,14 @@ def upload():
         upload_path = os.path.join(basepath, 'static/uploads',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
         f.save(upload_path)
         return redirect(url_for('upload'))
-    return render_template('upload.html')
+    return render_template('upload.html',
+                           title='上传文件',
+        year=datetime.now().year,)
 
 @app.route('/about')
 def about():
     """Renders the about page."""
-    return render_template(
-        'about.html',
+    return render_template('about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
-    )
+        message='Your application description page.')
